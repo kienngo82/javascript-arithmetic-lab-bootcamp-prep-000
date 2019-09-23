d8a943aaded15d189560e030748240dbfe1621ba
Kien Ngo <kienngo82@gmail.com>
2019-09-23 06:37:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,15 @@ function makeInt(n)
   return parseInt(n, 10);
 }
 
+function preserveDecimal(n)
+{
+  return parseFloat(n);
+}
 
+function preserveDecimal(n)
+{
+  return parseFloat(n, 10);
+}
 
 
 
